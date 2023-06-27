3440c17c1722349298d81c6efba8f50ae050eafe
CaterinaFro <127872343+CaterinaFro@users.noreply.github.com>
2023-06-27 18:50:18 +0200

    service-providers-dbls

--- src/root/root.component.ts
+++ src/root/root.component.ts
@@ -3,18 +3,22 @@ import { NoleggioComponent } from './noleggio/noleggio.component';
 import {RicercaComponent} from './ricerca/ricerca.component';
 import {InserimentoComponent} from './inserimento/inserimento.component';
 import {CommonModule} from '@angular/common'
+import { DbLibriService } from './db-libri.service';
+import { AjaxResponse } from 'rxjs/ajax';
 
 @Component({
   selector: 'app-root',
   templateUrl: './root.component.html',
   styleUrls: ['./root.component.css'],
   standalone: true,
-  imports: [RicercaComponent, CommonModule, InserimentoComponent]
+  imports: [RicercaComponent, CommonModule, InserimentoComponent],
+  providers: [DbLibriService],
 })
 export class RootComponent implements OnInit {
   view: string = 'home';
   sez: boolean = true;
-  constructor() { }
+  
+  constructor(private dbls: DbLibriService) { }
 
 
   ngOnInit() {
